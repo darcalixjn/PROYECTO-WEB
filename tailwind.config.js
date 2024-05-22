83ae677e39b0d23b0e21c7c27c754a2733f3c6d2
darcalixjn <149524133+darcalixjn@users.noreply.github.com>
2024-05-21 22:03:16 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+    ".pages/**/*.{html,ts}",
+    ".componentes/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
